15691bb602cdcdc5fa8e2927de6d0ae3913d3e57
arif <arifbillahmubin@gmail.com>
2025-12-03 19:35:58 +0600

    tanstack query added in add-plant component

--- src/components/Form/AddPlantForm.jsx
+++ src/components/Form/AddPlantForm.jsx
@@ -1,7 +1,38 @@
 import { useForm } from "react-hook-form"
+import { imageUpload } from "../../../utils";
+import useAuth from "../../hooks/useAuth";
+import axios from "axios";
+import { useMutation } from "@tanstack/react-query";
+import LoadingSpinner from "../Shared/LoadingSpinner";
+import ErrorPage from "../../pages/ErrorPage";
+import toast from "react-hot-toast";
+import { TbFidgetSpinner } from "react-icons/tb";
 
 const AddPlantForm = () => {
+  const { user } = useAuth();
 
+  //useMutation use case for tanstack query
+  const {isPending,isError,mutateAsync,reset:mutationReset} = useMutation({
+    mutationFn: async payload => await axios.post(`${import.meta.env.VITE_API_URL}/plants`, payload),
+    onSuccess: data => {
+      console.log(data)
+      toast.success('Plant added successfully')
+      mutationReset();
+    },
+    onError: error => {
+      console.log(error)
+    },
+    onMutate: payload => {
+      console.log('i will post this data :', payload)
+    },
+    onSettled: (data, error) => {
+      if (data) console.log(data)
+      if (error) console.log(error)
+    },
+    retry: 3
+  })
+
+  //react hook from
   const {
     register,
     handleSubmit,
@@ -9,14 +40,41 @@ const AddPlantForm = () => {
     formState: { errors }
   } = useForm()
 
-  const onSubmit = (data) => {
-    console.log("Submitted Plant Data:", data)
+  const onSubmit = async data => {
+    const { image, name, category, price, quantity, description } = data;
 
-    // You can handle API POST request here
-    // axios.post('/api/plant', data)
+    const imageFile = image[0];
 
+    try {
+      const imageURL = await imageUpload(imageFile);
+
+      const plantData = {
+        name,
+        image: imageURL,
+        description,
+        category,
+        price: Number(price),
+        quantity: Number(quantity),
+        seller: {
+          image: user?.photoURL,
+          name: user?.displayName,
+          email: user?.email
+        }
+      }
+
+    //use tanstack query
+    await mutateAsync(plantData);
+
+    } catch (err) {
+      console.log(err);
+    }
+    // console.log(user);
+    // console.table(plantData);
     reset()
   }
+
+  if(isPending) <LoadingSpinner></LoadingSpinner>
+  if(isError) <ErrorPage></ErrorPage>
 
   return (
     <div className='w-full min-h-[calc(100vh-40px)] flex flex-col justify-center items-center text-gray-800 rounded-xl bg-gray-50'>
@@ -96,7 +154,11 @@ const AddPlantForm = () => {
                 </label>
                 <input
                   {...register("price", {
-                    required: "Price is required."
+                    required: "Price is required.",
+                    min: {
+                      value: 0,
+                      message: "price must be positive number."
+                    }
                   })}
                   className='w-full px-4 py-3 text-gray-800 border border-lime-300 rounded-md bg-white focus:outline-lime-500'
                   id='price'
@@ -117,7 +179,11 @@ const AddPlantForm = () => {
                 </label>
                 <input
                   {...register("quantity", {
-                    required: "Quantity is required."
+                    required: "Quantity is required.",
+                    min: {
+                      value: 0,
+                      message: 'Quantity must be positive number'
+                    }
                   })}
                   className='w-full px-4 py-3 text-gray-800 border border-lime-300 rounded-md bg-white focus:outline-lime-500'
                   id='quantity'
@@ -161,7 +227,10 @@ const AddPlantForm = () => {
               type='submit'
               className='w-full cursor-pointer p-3 mt-5 text-center font-medium text-white rounded shadow-md bg-lime-500 hover:bg-lime-600'
             >
-              Save & Continue
+              {isPending ?
+                <TbFidgetSpinner className="animate-spin m-auto"></TbFidgetSpinner>:
+                'Save & Continue'
+              }
             </button>
           </div>
         </div>
