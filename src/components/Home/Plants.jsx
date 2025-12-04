d117ddbb72025d57133c6db352ab23abb0ead970
arif <arifbillahmubin@gmail.com>
2025-12-04 14:57:59 +0600

    plant data fetch in home page from useQuery

--- src/components/Home/Plants.jsx
+++ src/components/Home/Plants.jsx
@@ -1,23 +1,33 @@
 import Card from './Card'
 import Container from '../Shared/Container'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+import LoadingSpinner from '../Shared/LoadingSpinner'
 
 const Plants = () => {
+
+  //use case useQuery for tanstack query
+  const {data : plants=[], isLoading} = useQuery({
+    queryKey: ['plants'],
+    queryFn: async () => {
+      const result = await axios(`${import.meta.env.VITE_API_URL}/plants`);
+      return result.data
+    }
+  })
+  // console.log(plants,isLoading);
+
+  if(isLoading) return <LoadingSpinner></LoadingSpinner>
+
   return (
     <Container>
-      <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-      </div>
+      {
+        plants ? (
+        <div className='pt-12 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
+          {
+            plants.map(plant => <Card key={plant._id} plant={plant} />)
+          }
+        </div> ): null
+      }
     </Container>
   )
 }
