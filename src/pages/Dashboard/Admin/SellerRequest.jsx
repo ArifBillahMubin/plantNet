0ac2099f774507fb04156e197bc9c5e70f612783
arif <arifbillahmubin@gmail.com>
2025-12-10 02:49:34 +0600

    seller req done

---
+++ src/pages/Dashboard/Admin/SellerRequest.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import SellerRequestDataRow from '../../../components/Dashboard/TableRows/sellerRequestDataRow';
+import useAuth from '../../../hooks/useAuth';
+import useAxiosSecure from '../../../hooks/useAxiosSecure';
+import { useQuery } from '@tanstack/react-query';
+import LoadingSpinner from '../../../components/Shared/LoadingSpinner';
+
+const SellerRequest = () => {
+    const { user } = useAuth()
+    const axiosSecure = useAxiosSecure()
+
+    const { data: request = [], isLoading } = useQuery({
+        queryKey: ['seller-request', user?.email],
+        queryFn: async () => {
+            const result = await axiosSecure(`/seller-request`);
+            return result.data
+        }
+    })
+
+    if(isLoading) return <LoadingSpinner></LoadingSpinner>
+    return (
+        <>
+            <div className='container mx-auto px-4 sm:px-8'>
+                <div className='py-8'>
+                    <div className='-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto'>
+                        <div className='inline-block min-w-full shadow rounded-lg overflow-hidden'>
+                            <table className='min-w-full leading-normal'>
+                                <thead>
+                                    <tr>
+                                        <th
+                                            scope='col'
+                                            className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                                        >
+                                            Email
+                                        </th>
+                                        <th
+                                            scope='col'
+                                            className='px-5 py-3 bg-white  border-b border-gray-200 text-gray-800  text-left text-sm uppercase font-normal'
+                                        >
+                                            Action
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        request.map(request => <SellerRequestDataRow key={request._id} req={request}></SellerRequestDataRow>)
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default SellerRequest;
